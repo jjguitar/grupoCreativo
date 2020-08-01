207522f388f95898c3e17ceba55ee1ce4049a513
Jhoncito el pillo <jhon.electronica@gmail.com>
2020-08-01 00:11:32 -0500

    Animation services

--- src/templates/Product.js
+++ src/templates/Product.js
@@ -1,10 +1,11 @@
 import '../assets/styles/Product.scss'
+import '../assets/styles/Lightbox.scss'
 
 const Product = ( tittle, img, description) => {
   const view = `
   <div class="Product">
     <img class="Product--img" src="${img}">
-    <h2>${tittle}</h2>
+    <h2 class="hide">${tittle}</h2>
     <div class="overlay">
       <h2 class="Product--tittle">${tittle}</h2>
       <p class="Product--description">${description}</p>
